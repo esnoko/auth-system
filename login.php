5f7125d97796b3d9a88b7274d41ee270599abdc3
esnoko <elvisnoko18@gmail.com>
2025-09-01 23:54:56 +0200

    add name attributes to email and password input fields in login form

--- login.php
+++ login.php
@@ -24,11 +24,11 @@
         <h1 class="h3 mt-5 fw-normal text-center">Please Login</h1>
 
         <div class="form-floating">
-            <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com">
+            <input name="email" type="email" class="form-control" id="floatingInput" placeholder="name@example.com">
             <label for="floatingInput">Email address</label>
         </div>
         <div class="form-floating">
-            <input type="password" class="form-control" id="floatingPassword" placeholder="Password">
+            <input name="password" type="password" class="form-control" id="floatingPassword" placeholder="Password">
             <label for="floatingPassword">Password</label>
         </div>
 
