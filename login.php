853db646267641897b3fb3ea76c800b8309c73d9
esnoko <elvisnoko18@gmail.com>
2025-09-01 23:53:42 +0200

    add registration logic and validation to login.php

--- login.php
+++ login.php
@@ -14,6 +14,22 @@
 
      // And use the paasword_verify function
 
+     if(isset($_POST['submit'])) {
+        IF($_POST['email'] == '' OR $_POST['username'] == '' OR $_POST['password'] == '') {
+            echo "<div class='alert alert-danger'>Please fill in all fields</div>";
+        } else {
+            $email = $_POST['email'];
+            $username = $_POST['username'];
+            $password = password_hash($_POST['password'], PASSWORD_DEFAULT);            // Insert into database
+            $stmt = $conn->prepare("INSERT INTO users (email, username, mypassword) VALUES (?, ?, ?)");
+            if($stmt->execute([$email, $username, $password])) {
+                echo "<div class='alert alert-success'>Registration successful!</div>";
+            } else {
+                echo "<div class='alert alert-danger'>Registration failed. Please try again.</div>";
+            }
+        }
+      }
+
 ?>
 <main class="form-signin w-50 m-auto">
     <form>
@@ -28,7 +44,7 @@
             <label for="floatingPassword">Password</label>
         </div>
 
-        <button class="w-100 btn btn-lg btn-primary" type="submit">Sign in</button>
+        <button name="submit" class="w-100 btn btn-lg btn-primary" type="submit">Sign in</button>
         <h6 class="mt-3">Don't have an account <a href="register.php">Create your account</a></h6>
     </form>
 </main>
